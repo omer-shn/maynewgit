129163fabef5cc576d6bcb29be25a6a9468619f7
omer-shn <omershnopium@gmail.com>
2022-05-10 15:17:48 +0300

    nomore commit

---
+++ src/MayForStoreHous.java
@@ -0,0 +1,5 @@
+public class MayForStoreHous {
+    public static void main(String[] args) {
+        //This is for storehouse
+    }
+}
--- src/Maymay.java
+++ src/Maymay.java
@@ -1,5 +1,6 @@
 public class Maymay {
     public static void main(String[] args) {
         //This is maymay proje
+        //This is static Headline
     }
 }
