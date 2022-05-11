70017b14671486270ef1fa1389c037e1c5e34434
= <omershnopium@gmail.com>
2022-05-11 11:06:20 +0300

    mayeleven

--- src/MayForStoreHous.java
+++ src/MayForStoreHous.java
@@ -1,5 +1,7 @@
 public class MayForStoreHous {
     public static void main(String[] args) {
         //This is for storehouse
+        //this class will be deleted soon
+        //this is new process
     }
 }
--- src/Maymay.java
+++ src/Maymay.java
@@ -2,5 +2,6 @@ public class Maymay {
     public static void main(String[] args) {
         //This is maymay proje
         //This is static Headline
+        //This is new comment
     }
 }
